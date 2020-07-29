309873d89869060e615ffa380a9294de9c3261d6
Osakpolor Obaseki <obasekiosa@gmail.com>
2020-07-29 14:05:58 +0100

    Refactor run.py

--- run.py
+++ run.py
@@ -7,11 +7,11 @@ import configparser
 config = configparser.ConfigParser()
 config.read(os.path.abspath(os.path.join(".ini")))
 
-if __name__ == "__main__":
-    app = create_app()
-    app.config['DEBUG'] = True
-    app.config['MFLIX_DB_URI'] = config['PROD']['MFLIX_DB_URI']
-    app.config['MFLIX_NS'] = config['PROD']['MFLIX_NS']
-    app.config['SECRET_KEY'] = config['PROD']['SECRET_KEY']
+app = create_app()
+app.config['DEBUG'] = True
+app.config['MFLIX_DB_URI'] = config['PROD']['MFLIX_DB_URI']
+app.config['MFLIX_NS'] = config['PROD']['MFLIX_NS']
+app.config['SECRET_KEY'] = config['PROD']['SECRET_KEY']
 
+if __name__ == "__main__":
     app.run()
